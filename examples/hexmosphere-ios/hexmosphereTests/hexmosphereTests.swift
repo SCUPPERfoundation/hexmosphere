224a5f9f3bcc3ec5b71781cfb45f3d5524396572
David Van Duzer <dvd@tennica.net>
2015-05-25 20:02:26 -0600

    setting up full demo app

---
+++ examples/hexmosphere-ios/hexmosphereTests/hexmosphereTests.swift
@@ -0,0 +1,36 @@
+//
+//  hexmosphereTests.swift
+//  hexmosphereTests
+//
+//  Created by David Van Duzer on 5/25/15.
+//  Copyright (c) 2015 David Van Duzer. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class hexmosphereTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
